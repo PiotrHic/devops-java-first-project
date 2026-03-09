4351a5604dba5d123e6ae8562fbb0579219558ac
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-03-09 17:31:21 +0100

    first commit

---
+++ src/test/java/org/example/devopsjavafirstproject/DevopsJavaFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devopsjavafirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJavaFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
