35993e7b38a640d8005fe872663d188cda059908
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-03-09 17:46:10 +0100

    Initial commit

--- src/main/java/org/example/devopsjavafirstproject/DevopsJavaFirstProjectApplication.java
+++ src/main/java/org/example/devopsjavafirstproject/DevopsJavaFirstProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsJavaFirstProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsJavaFirstProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps from Java!");
+    }
 
 }
--- src/test/java/org/example/devopsjavafirstproject/DevopsJavaFirstProjectApplicationTests.java
+++ src/test/java/org/example/devopsjavafirstproject/DevopsJavaFirstProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DevopsJavaFirstProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads () {
+    }
 
 }
